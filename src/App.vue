5a68a99a5b30671c562d67cd3d9bdc5b3ddb39e3
nhquang192 <nhquang192@gmail.com>
2022-06-21 14:18:33 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,168 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app overflow-x-hidden">
+    <!-- Section1 -->
+    <div class="h-[500px] bg-mygradiant">
+      <header class="px-6 py-[30px] flex items-center justify-between">
+        <img src="./assets/logo.svg" alt="" />
+        <ul class="flex gap-x-14 items-center">
+          <li class="text-primary-color">Home</li>
+          <li>Explore</li>
+          <li>
+            <button class="py-2 px-9 bg-primary-color text-white">
+              Get Started
+            </button>
+          </li>
+        </ul>
+      </header>
+      <div class="ml-80 mt-36">
+        <h2 class="text-5xl font-bold max-w-3xl">
+          Your strategy is only as good as you execute it.
+        </h2>
+        <button class="py-2 px-9 mt-4 bg-primary-color text-white">
+          Get Started
+        </button>
+      </div>
+    </div>
+    <!-- Section2 -->
+    <div class="h-[500px]">
+      <div class="mx-[150px] my-[60px] flex flex-col items-center">
+        <h2 class="font-bold text-3xl">
+          Company Profile<span class="text-primary-color">.</span>
+        </h2>
+        <span class="text-primary-color text-base">short introduction</span>
+        <div class="mt-[60px] flex items-center gap-x-4">
+          <img src="./assets/Group1000002554.png" alt="" class="w-1/2" />
+          <p class="leading-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- Section3 -->
+    <div class="h-[600px] bg-mygradiant">
+      <div class="mx-[150px] flex flex-col items-center">
+        <span class="mt-[60px] text-primary-color text-base"
+          >short introduction</span
+        >
+        <h2 class="font-bold text-3xl">
+          Dream big Do right<span class="text-primary-color">.</span>
+        </h2>
+        <div class="mt-[60px] flex items-center gap-x-[50px]">
+          <!-- icon1 -->
+          <div
+            class="w-[250px] m-auto p-5 flex flex-col items-center bg-white rounded-md"
+          >
+            <img src="./assets/icon1.svg" alt="" />
+            <p>Free shipping</p>
+            <p class="text-center mt-4 text-gray-700">
+              Enjoy Order in a hand using the fresness of groceries
+            </p>
+          </div>
+          <!-- icon2 -->
+          <div class="relative w-[250px] z-10">
+            <div class="relative bg-white rounded-md flex z-10">
+              <div class="m-5 flex flex-col items-center">
+                <img src="./assets/icon2.svg" alt="" />
+                <p>Free shipping</p>
+                <p class="text-center mt-4 text-gray-700">
+                  Enjoy Order in a hand using the fresness of groceries
+                </p>
+              </div>
+            </div>
+            <div
+              class="absolute w-full h-full top-3 left-4 transform -rotate-2 rounded-md bg-primary-color z-0"
+            ></div>
+          </div>
+          <!-- icon3 -->
+          <div
+            class="w-[250px] m-auto p-5 flex flex-col items-center bg-white rounded-md"
+          >
+            <img src="./assets/icon3.svg" alt="" />
+            <p>Free shipping</p>
+            <p class="text-center mt-4 text-gray-700">
+              Enjoy Order in a hand using the fresness of groceries
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Section4 -->
+    <div class="h-[500px] bg-[#F2F2F2]">
+      <div class="mx-[150px] pt-[60px] flex flex-col items-center">
+        <h2 class="font-bold text-3xl">
+          Our Amazing Partners<span class="text-primary-color">.</span>
+        </h2>
+        <span class="text-primary-color text-base">short introduction</span>
+        <div
+          class="mt-[60px] grid grid-rows-3 grid-flow-col items-center gap-y-3 gap-x-6"
+        >
+          <img src="./assets/logo-behance.png" alt="" />
+          <img src="./assets/logo-uplabs.svg" alt="" />
+          <img src="./assets/logo-behance.png" alt="" />
+          <img src="./assets/logo-google.png" alt="" />
+          <img src="./assets/logo-99designs.svg" alt="" />
+          <img src="./assets/logo-uplabs.svg" alt="" />
+          <img src="./assets/logo-uplabs.svg" alt="" />
+          <img src="./assets/logo-behance.png" alt="" />
+          <img src="./assets/logo-google.png" alt="" />
+          <img src="./assets/logo-dribbble.png" alt="" />
+          <img src="./assets/logo-google.png" alt="" />
+          <img src="./assets/logo-dribbble.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- Section5 -->
+    <div
+      class="h-[400px] px-[150px] my-[60px] flex flex-row items-center justify-between"
+    >
+      <div class="flex flex-col w-2/5">
+        <h2 class="font-bold text-3xl">
+          Customer service<span class="text-primary-color">.</span>
+        </h2>
+        <span class="text-primary-color text-base">short introduction</span>
+        <span class="mt-[40px]">+84 - 1234567890</span>
+        <span>finance@gmail.com</span>
+        <span>Rockefeller PlazaNew York, NY 10112</span>
+      </div>
+      <img src="./assets/ggmap.png" alt="" class="w-3/5" />
+    </div>
+    <!-- Section6 -->
+    <div>
+      <div class="h-[400px] bg-[#F2F2F2]">
+        <div class="mx-[150px] pt-[60px] flex flex-col items-center">
+          <h2 class="font-bold text-3xl">
+            Our Amazing Partners<span class="text-primary-color">.</span>
+          </h2>
+          <span class="text-primary-color text-base">short introduction</span>
+          <div
+            class="mt-[60px] grid grid-rows-3 grid-flow-col items-center gap-y-3 gap-x-6"
+          >
+            <img src="./assets/logo-dribbble.png" alt="" />
+            <img src="./assets/logo-google.png" alt="" />
+            <img src="./assets/logo-dribbble.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer></footer>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,ts,html}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#F79901",
+      },
+    },
+  },
+  plugins: [],
+};
